238d2c9573df719ba630d4dad7cb54c7b2a12b51
Tomáš Kordoš <kordo.t12@gmail.com>
2025-10-12 15:08:41 +0200

    handlers, structure

--- src/components/CommandLine/Open.ts
+++ src/components/CommandLine/Open.ts
@@ -1,7 +1,11 @@
 export default function Open() {
   // inject currentOpenChannel ref from parent component
+  // take it as parameter to the Open() function from commandline
+  // use it there to update the UI for kick, etc.
   return {
     cmd: 'open',
-    execute: (args: string[]) => {},
+    execute: (args: string[]) => {
+      console.log('Open command executed with args:', args);
+    },
   };
 }
