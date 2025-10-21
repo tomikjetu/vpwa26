022f1e1410ad0c9d501292d539e814dcd95266d9
Tomáš Kordoš <kordo.t12@gmail.com>
2025-10-21 14:18:23 +0200

    dark cli

--- src/components/CommandLine/CommandLine.vue
+++ src/components/CommandLine/CommandLine.vue
@@ -206,4 +206,9 @@ onBeforeUnmount(() => window.removeEventListener('keydown', onKeydown));
     border-radius: 8px;
     box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
 }
+
+.body--dark .cli-wrapper {
+    background: #1e1e1e;
+    color: white;
+}
 </style>
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -11,29 +11,36 @@ export type Channel = {
   infoColor: string;
   isPublic: boolean;
   hasUnreadMsgs: boolean;
-  members:  Record<number, Member>;
+  members: Record<number, Member>;
 };
+
+/*
+  Currently logged in user
+*/
 export type User = {
-  id: number;
+  id: number /** contact id */;
   name: string;
   surname: string;
   nickName: string;
   email: string;
 };
 
+/*
+  Every contact, including the logged in user contact
+*/
 export type Contact = {
-  id: number;
+  id: number /** contact id */;
   status: string;
-}
+};
 
 export type Member = {
-  id: number;
+  id: number /** contact id */;
   nickname: string;
   kickVotes: number;
   isOwner: boolean;
   currentlyTyping?: string;
   kickVoters: number[];
-}
+};
 
 export type LoginCredentials = {
   email: string;
@@ -50,15 +57,15 @@ export type RegisterCredentials = {
 };
 
 export interface DropdownItem {
-  label: string,
-  class: string,
-  disable: boolean
+  label: string;
+  class: string;
+  disable: boolean;
 }
 
 export interface ChatMessagePayload {
-  user: number,
-  text: string,
-  time: Date,
-  files: (File | string)[],
-  userNickname: string
+  user: number;
+  text: string;
+  time: Date;
+  files: (File | string)[];
+  userNickname: string;
 }
